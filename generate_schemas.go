c139076dbab96e2ffd543d961d63e54eb8af2871
Pete Savage <psav@users.noreply.github.com>
2025-01-20 21:34:52 +0000

    Updated to new CRD version (#3)
    * Updated to new CRD version
    
    * Do not modify schema before passing to gojsonschema
    
    ---------
    
    Co-authored-by: Brandon Squizzato <bsquizza@redhat.com>

--- generate_schemas.go
+++ generate_schemas.go
@@ -3,12 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
 
 	"encoding/json"
 
-	apiextv1beta1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,11 +20,11 @@ import (
 
 // JSONSchema describes json schema file
 type JSONSchema struct {
-	Schema      string                                   `json:"$schema"`
-	Ref         string                                   `json:"$ref"`
-	ID          string                                   `json:"$id"`
-	Title       string                                   `json:"$title"`
-	Definitions map[string]apiextv1beta1.JSONSchemaProps `json:"definitions"`
+	Schema      string                              `json:"$schema"`
+	Ref         string                              `json:"$ref"`
+	ID          string                              `json:"$id"`
+	Title       string                              `json:"$title"`
+	Definitions map[string]apiextv1.JSONSchemaProps `json:"definitions"`
 }
 
 var sch = runtime.NewScheme()
@@ -32,10 +32,10 @@ var sch = runtime.NewScheme()
 func init() {
 	metav1.AddToGroupVersion(sch, schema.GroupVersion{Version: "v1"})
 	utilruntime.Must(scheme.AddToScheme(sch))
-	utilruntime.Must(apiextv1beta1.AddToScheme(sch))
+	utilruntime.Must(apiextv1.AddToScheme(sch))
 }
 
-func decodeCRD(jsonBytes []byte) (*apiextv1beta1.CustomResourceDefinition, error) {
+func decodeCRD(jsonBytes []byte) (*apiextv1.CustomResourceDefinition, error) {
 	// decode as an unstructured object and then cast to CRD, since I
 	// was having issues decoding straight to the CRD obj.
 	// See https://github.com/openshift/origin/pull/21936/files
@@ -58,7 +58,7 @@ func decodeCRD(jsonBytes []byte) (*apiextv1beta1.CustomResourceDefinition, error
 		return nil, fmt.Errorf("unable to convert unstructured object to '%v': %v", gvk, err)
 	}
 
-	crd, ok := newObj.(*apiextv1beta1.CustomResourceDefinition)
+	crd, ok := newObj.(*apiextv1.CustomResourceDefinition)
 	if !ok {
 		return nil, fmt.Errorf("unable to cast %T to *apiextv1beta1.CustomResourceDefinition", obj)
 	}
@@ -68,7 +68,7 @@ func decodeCRD(jsonBytes []byte) (*apiextv1beta1.CustomResourceDefinition, error
 
 func generate(in *string, out *string) {
 	// read yaml
-	yb, err := ioutil.ReadFile(*in)
+	yb, err := os.ReadFile(*in)
 	if err != nil {
 		log.Fatalf("failed to read file: %v", err)
 	}
@@ -83,6 +83,7 @@ func generate(in *string, out *string) {
 	}
 
 	kind := crd.Spec.Names.Kind
+	fmt.Printf("parsing %s\n", kind)
 
 	schema := &JSONSchema{
 		Schema: "http://json-schema.org/draft-07/schema#",
@@ -90,17 +91,16 @@ func generate(in *string, out *string) {
 		ID:     kind,
 		Title:  kind,
 		//Definitions: make(map[string]JSONSchemaDefinition),
-		Definitions: make(map[string]apiextv1beta1.JSONSchemaProps),
+		Definitions: make(map[string]apiextv1.JSONSchemaProps),
 	}
-	crd.Spec.Validation.OpenAPIV3Schema.Type = "object"
-	crd.Spec.Validation.OpenAPIV3Schema.Description = kind
-	schema.Definitions[kind] = *crd.Spec.Validation.OpenAPIV3Schema
+
+	schema.Definitions[kind] = *crd.Spec.Versions[0].Schema.OpenAPIV3Schema
 
 	output, err := json.MarshalIndent(schema, "", "  ")
 	if err != nil {
 		log.Fatalf("failed to marshal json: %v", err)
 	}
-	err = ioutil.WriteFile(*out, output, 0644)
+	err = os.WriteFile(*out, output, 0644)
 	if err != nil {
 		log.Fatalf("failed to write file: %v", err)
 	}
